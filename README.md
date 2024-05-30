e549b95b8bd4c5c0e11cde5dd3abc853f7108160
RobertGarabetian <robertgarabetian90@gmail.com>
2024-05-30 12:25:33 -0700

    Set up graph ql with apollo

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+- [x] Set-up React
+- [x] Tailwind and DaisyUI
+- [x] Set-up Graphql
+- [] Decide which API to use
+- [] Do I need a .env file??
+- [] Decide on a database (SQl or Nosql??)
+- [] Set-up Go Backend
+- [] User Auth (Haven't decided how yet)
+- [] 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+client.query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+  <App />
+</ApolloProvider>,
 )
